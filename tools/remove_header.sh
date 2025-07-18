0a5f37572b5f53b49397cfef09d33ba1ba1913a2
Russian95CrE <russian95@infernointeractive.win>
2025-07-18 13:37:44 -0300

    Fix remove_header.sh to work with the new header

--- tools/remove_header.sh
+++ tools/remove_header.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
 find . -type f \( -name "*.c" -o -name "*.h" \) | while read -r file; do
-    if grep -q "Wind/Tempest is free software" "$file"; then
-        echo "Removing header of: $file"
-        perl -0777 -i -pe '
-            s{
-                \A
-                (?:/\* [^*]*? \*/\R)?
-                /\*
-                .*?Wind/Tempest\ is\ free\ software
-                .*?\*/
-                (?:\R){0,2}
-            }{}xms;
-        ' "$file"
-        echo "Header removed!"
-    fi
+	if grep -q "Wind/Tempest" "$file"; then
+		echo "Removing header of: $file"
+
+		awk '
+			BEGIN { inside_comment = 0; header_skipped = 0; printed = 0 }
+
+			NR == 1 && /^\s*\/\*.*\*\/\s*$/ {
+				next
+			}
+
+			/^\/\*/ && header_skipped == 0 {
+				inside_comment = 1
+				comment_block = $0 "\n"
+				next
+			}
+
+			inside_comment {
+				comment_block = comment_block $0 "\n"
+				if ($0 ~ /Wind\/Tempest/) {
+					skip_this = 1
+				}
+				if ($0 ~ /\*\//) {
+					inside_comment = 0
+					if (skip_this) {
+						header_skipped = 1
+						skip_this = 0
+						next
+					} else {
+						printf "%s", comment_block
+					}
+					comment_block = ""
+					next
+				}
+				next
+			}
+
+			{
+				if (!printed && $0 ~ /^[[:space:]]*$/) next
+				printed = 1
+				print
+			}
+		' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+
+		echo "Header removed!"
+	fi
 done
