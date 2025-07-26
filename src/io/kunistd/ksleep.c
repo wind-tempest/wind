bac6fcd8cf840c1644e4662d642152d18d64c59f
Russian95CrE <russian95@infernointeractive.win>
2025-07-26 01:56:24 -0300

    Update LICENSE in LICENSE header
    Date: 2025-07-26 04:56:24 UTC
    Signed-off-by: Russian95CrE <russian95@infernointeractive.win>

--- src/io/kunistd/ksleep.c
+++ src/io/kunistd/ksleep.c
@@ -1,15 +1,17 @@
 // ksleep.c
 
-// SPDX-License-Identifier: LSL-1.0
+// SPDX-License-Identifier: LSL-1.1
 /*
  * Wind/Tempest Project
  *
  * Copyright (C) 2025 Wind Foundation <https://wind.infernointeractive.win>
  *
  * This file is part of the Wind Operating System and Tempest Kernel.
- * It is licensed under the Liberty Software License 1.0 (LSL-1.0).
+ * It is licensed under the Liberty Software License 1.1 (LSL-1.1).
  * For more information, see <https://wind.infernointeractive.win>.
  */
+
+// SPDX-License-Identifier: LSL-1.0
 
 #include "ksleep.h"
 
--- tools/header.sh
+++ tools/header.sh
@@ -3,14 +3,14 @@
 headers_bad=false
 
 read -r -d '' license_header <<'EOF'
-// SPDX-License-Identifier: LSL-1.0
+// SPDX-License-Identifier: LSL-1.1
 /*
  * Wind/Tempest Project
  *
  * Copyright (C) 2025 Wind Foundation <https://wind.infernointeractive.win>
  *
  * This file is part of the Wind Operating System and Tempest Kernel.
- * It is licensed under the Liberty Software License 1.0 (LSL-1.0).
+ * It is licensed under the Liberty Software License 1.1 (LSL-1.1).
  * For more information, see <https://wind.infernointeractive.win>.
  */
 EOF
