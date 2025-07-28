f77569658de9ba9e7e29c59c3cea35fc2f919795
Russian95CrE <russian95@tempestfoundation.org>
2025-07-28 19:58:15 -0300

    Remove useless cast in kmemcmp.c

--- kernel/klibc/kstring/kmemcmp.c
+++ kernel/klibc/kstring/kmemcmp.c
@@ -14,11 +14,11 @@
 
 int
     kmemcmp (const void *s1, const void *s2, unsigned long n) {
-	const unsigned char *a = (const unsigned char *) s1;
-	const unsigned char *b = (const unsigned char *) s2;
+	const unsigned char *a = s1;
+	const unsigned char *b = s2;
 	for ( unsigned long i = 0; i < n; ++i ) {
 		if ( a[i] != b[i] )
-			return (int) a[i] - (int) b[i];
+			return a[i] - b[i];
 	}
 	return 0;
 }
