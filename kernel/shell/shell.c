0695e4d88ae11779fa36bd0711b2d91f18e4982d
Russian95CrE <russian95@tempestfoundation.org>
2025-07-29 22:39:57 -0300

    shell: Replace "user-not-implemented-yet" to "kernel"
    Replace "user-not-implemented-yet" to "kernel" to know the kernel (for now)
    is on kernel space (AKA: ring 0)
    
    Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

--- kernel/shell/shell.c
+++ kernel/shell/shell.c
@@ -138,7 +138,7 @@ void
 	kputs("Type 'help' for a list of commands.");
 
 	while ( ktrue ) {
-		kprintf("\n$[user-not-implemented-yet] ");
+		kprintf("\n$[kernel] "); // use kernel for determining it's kernel space (AKA: ring 0)
 		cmd_ptr	       = 0;
 		input_overflow = 0;
 
