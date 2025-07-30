wind-tempest/wind

commit 7f0820b73e70419e34b0d662639a3b160a76a5ed

fs: Make filesize limit to 268 KiB
Fixes: 7e225df3d344940f7e21b1451087b7aae5862c33 ("fs: Allow support for >4 GiB files")
Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

kernel/fs/ext2/ext2.c
@@ -431,6 +431,46 @@ int
 	return EXT2_OK;
 }
 
+// Helper function to get the physical block ID for a logical block within a file
+static kuint32_t
+    kget_file_block (const ext2_inode_t *inode, kuint64_t logical_block) {
+	kuint32_t blocks_per_indirect = g_block_size / sizeof(kuint32_t);
+
+	// Direct blocks (0-11)
+	if ( logical_block < 12 ) {
+		return inode->block[logical_block];
+	}
+
+	// Single indirect blocks (12 to 12 + blocks_per_indirect - 1)
+	kuint64_t single_indirect_start = 12;
+	kuint64_t single_indirect_end	= single_indirect_start + blocks_per_indirect;
+
+	if ( logical_block < single_indirect_end ) {
+		kuint32_t indirect_block = inode->block[12];
+		if ( indirect_block == 0 )
+			return 0; // Sparse
+
+		// Read the indirect block
+		kuint32_t *indirect_buf = (kuint32_t *) kmalloc(g_block_size);
+		if ( !indirect_buf )
+			return 0;
+
+		if ( kread_block(indirect_block, indirect_buf) != 0 ) {
+			kfree(indirect_buf);
+			return 0;
+		}
+
+		kuint32_t index	 = (kuint32_t) (logical_block - single_indirect_start);
+		kuint32_t result = indirect_buf[index];
+		kfree(indirect_buf);
+		return result;
+	}
+
+	// Double indirect blocks (not implemented yet)
+	// For now, only support up to single indirect
+	return 0;
+}
+
 int
     kext2_read (ext2_file_t *file, void *buf, ksize_t len) {
 	if ( !file || !buf )
@@ -454,12 +494,19 @@ int
 	while ( remaining > 0 ) {
 		kuint64_t block_idx    = file->pos / g_block_size;
 		kuint32_t off_in_block = (kuint32_t) (file->pos % g_block_size);
-		if ( block_idx >= 12 ) {
-			// Indirect blocks not supported yet
+
+		// Calculate maximum supported blocks
+		kuint32_t blocks_per_indirect = g_block_size / sizeof(kuint32_t);
+		kuint64_t max_blocks =
+		    12 + blocks_per_indirect; // Direct + single indirect
+
+		if ( block_idx >= max_blocks ) {
+			// Beyond single indirect blocks not supported yet
 			kfree(block_buf);
 			return EXT2_ERR_UNSUPPORTED;
 		}
-		kuint32_t blk_id = (block_idx < 12) ? file->inode.block[block_idx] : 0;
+
+		kuint32_t blk_id = kget_file_block(&file->inode, block_idx);
 		if ( blk_id == 0 ) {
 			// Sparse region – treat as zero
 			ksize_t chunk = g_block_size - off_in_block;

commit bc24ddf26c50d98f8930ea72f28cbb3c4f99c079

fs: Support up to 4 TiB in EXT2
Okay, now we support up to 4 TiB. Look, the EXT2 is just the initial FS to Wind (operating system)
and Tempest (the Kernel). We are gonna support various FS.

Fixes: 7f0820b73e70419e34b0d662639a3b160a76a5ed ("fs: Make filesize limit to 268 KiB")
Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

kernel/fs/ext2/ext2.c
@@ -466,8 +466,128 @@ static kuint32_t
 		return result;
 	}
 
-	// Double indirect blocks (not implemented yet)
-	// For now, only support up to single indirect
+	// Double indirect blocks
+	kuint64_t double_indirect_start = single_indirect_end;
+	kuint64_t double_indirect_end =
+	    double_indirect_start + (kuint64_t) blocks_per_indirect * blocks_per_indirect;
+
+	if ( logical_block < double_indirect_end ) {
+		kuint32_t double_indirect_block = inode->block[13];
+		if ( double_indirect_block == 0 )
+			return 0; // Sparse
+
+		// Calculate which single indirect block we need
+		kuint64_t offset_in_double = logical_block - double_indirect_start;
+		kuint32_t single_indirect_index =
+		    (kuint32_t) (offset_in_double / blocks_per_indirect);
+		kuint32_t data_block_index =
+		    (kuint32_t) (offset_in_double % blocks_per_indirect);
+
+		// Read the double indirect block to get the single indirect block pointer
+		kuint32_t *double_indirect_buf = (kuint32_t *) kmalloc(g_block_size);
+		if ( !double_indirect_buf )
+			return 0;
+
+		if ( kread_block(double_indirect_block, double_indirect_buf) != 0 ) {
+			kfree(double_indirect_buf);
+			return 0;
+		}
+
+		kuint32_t single_indirect_block =
+		    double_indirect_buf[single_indirect_index];
+		kfree(double_indirect_buf);
+
+		if ( single_indirect_block == 0 )
+			return 0; // Sparse
+
+		// Now read the single indirect block to get the data block pointer
+		kuint32_t *single_indirect_buf = (kuint32_t *) kmalloc(g_block_size);
+		if ( !single_indirect_buf )
+			return 0;
+
+		if ( kread_block(single_indirect_block, single_indirect_buf) != 0 ) {
+			kfree(single_indirect_buf);
+			return 0;
+		}
+
+		kuint32_t result = single_indirect_buf[data_block_index];
+		kfree(single_indirect_buf);
+		return result;
+	}
+
+	// Triple indirect blocks
+	kuint64_t triple_indirect_start = double_indirect_end;
+	kuint64_t triple_indirect_end =
+	    triple_indirect_start
+	    + (kuint64_t) blocks_per_indirect * blocks_per_indirect * blocks_per_indirect;
+
+	if ( logical_block < triple_indirect_end ) {
+		kuint32_t triple_indirect_block = inode->block[14];
+		if ( triple_indirect_block == 0 )
+			return 0; // Sparse
+
+		// Calculate indices for triple indirection
+		kuint64_t offset_in_triple = logical_block - triple_indirect_start;
+		kuint32_t double_indirect_index =
+		    (kuint32_t) (offset_in_triple
+				 / (blocks_per_indirect * blocks_per_indirect));
+		kuint32_t single_indirect_index =
+		    (kuint32_t) ((offset_in_triple
+				  % (blocks_per_indirect * blocks_per_indirect))
+				 / blocks_per_indirect);
+		kuint32_t data_block_index =
+		    (kuint32_t) (offset_in_triple % blocks_per_indirect);
+
+		// Read the triple indirect block to get the double indirect block pointer
+		kuint32_t *triple_indirect_buf = (kuint32_t *) kmalloc(g_block_size);
+		if ( !triple_indirect_buf )
+			return 0;
+
+		if ( kread_block(triple_indirect_block, triple_indirect_buf) != 0 ) {
+			kfree(triple_indirect_buf);
+			return 0;
+		}
+
+		kuint32_t double_indirect_block =
+		    triple_indirect_buf[double_indirect_index];
+		kfree(triple_indirect_buf);
+
+		if ( double_indirect_block == 0 )
+			return 0; // Sparse
+
+		// Read the double indirect block to get the single indirect block pointer
+		kuint32_t *double_indirect_buf = (kuint32_t *) kmalloc(g_block_size);
+		if ( !double_indirect_buf )
+			return 0;
+
+		if ( kread_block(double_indirect_block, double_indirect_buf) != 0 ) {
+			kfree(double_indirect_buf);
+			return 0;
+		}
+
+		kuint32_t single_indirect_block =
+		    double_indirect_buf[single_indirect_index];
+		kfree(double_indirect_buf);
+
+		if ( single_indirect_block == 0 )
+			return 0; // Sparse
+
+		// Finally read the single indirect block to get the data block pointer
+		kuint32_t *single_indirect_buf = (kuint32_t *) kmalloc(g_block_size);
+		if ( !single_indirect_buf )
+			return 0;
+
+		if ( kread_block(single_indirect_block, single_indirect_buf) != 0 ) {
+			kfree(single_indirect_buf);
+			return 0;
+		}
+
+		kuint32_t result = single_indirect_buf[data_block_index];
+		kfree(single_indirect_buf);
+		return result;
+	}
+
+	// Fuck you if you want more than triple indirect blocks.
 	return 0;
 }
 
@@ -498,10 +618,12 @@ int
 		// Calculate maximum supported blocks
 		kuint32_t blocks_per_indirect = g_block_size / sizeof(kuint32_t);
 		kuint64_t max_blocks =
-		    12 + blocks_per_indirect; // Direct + single indirect
-
+		    12 + blocks_per_indirect
+		    + (kuint64_t) blocks_per_indirect * blocks_per_indirect
+		    + (kuint64_t) blocks_per_indirect * blocks_per_indirect
+			  * blocks_per_indirect;
+		// Direct + single indirect + double indirect + triple indirect
 		if ( block_idx >= max_blocks ) {
-			// Beyond single indirect blocks not supported yet
 			kfree(block_buf);
 			return EXT2_ERR_UNSUPPORTED;
 		}