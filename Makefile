wind-tempest/wind

commit d737f3febfe3cab86b26a1d6f91a8467738fa56c

debug: Add subsystem in `kdbgtype()`
Add a subsystem option for `kdbgtype()` so we can find where the
subsystem is located.

Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

kernel/klibc/kdebug/kdebug.c
@@ -186,47 +186,54 @@ int
 }
 
 void
-    kdbgtype (const char *type, const char *message, const char *extra) {
+    kdbgtype (
+	const char *type, const char *subsystem, const char *message, const char *extra) {
 	if ( !message || *message == '\0' )
 		return;
 	if ( !extra || *extra == '\0' ) {
-		kprintf("[%s] %s\n", type, message);
+		if ( subsystem && *subsystem != '\0' )
+			kprintf("[%s][%s] %s\n", type, subsystem, message);
+		else
+			kprintf("[%s] %s\n", type, message);
 	} else {
-		kprintf("[%s] %s: %s\n", type, message, extra);
+		if ( subsystem && *subsystem != '\0' )
+			kprintf("[%s][%s] %s: %s\n", type, subsystem, message, extra);
+		else
+			kprintf("[%s] %s: %s\n", type, message, extra);
 	}
 }
 
 void
-    kcritical (const char *message, const char *extra) {
-	kdbgtype("CRITICAL", message, extra);
+    kcritical (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("CRITICAL", subsystem, message, extra);
 }
 
 void
-    kalert (const char *message, const char *extra) {
-	kdbgtype("ALERT", message, extra);
+    kalert (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("ALERT", subsystem, message, extra);
 }
 
 void
-    kemerg (const char *message, const char *extra) {
-	kdbgtype("EMERG", message, extra);
+    kemerg (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("EMERG", subsystem, message, extra);
 }
 
 void
-    kwarn (const char *message, const char *extra) {
-	kdbgtype("WARN", message, extra);
+    kwarn (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("WARN", subsystem, message, extra);
 }
 
 void
-    kerror (const char *message, const char *extra) {
-	kdbgtype("ERROR", message, extra);
+    kerror (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("ERROR", subsystem, message, extra);
 }
 
 void
-    knotice (const char *message, const char *extra) {
-	kdbgtype("NOTICE", message, extra);
+    knotice (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("NOTICE", subsystem, message, extra);
 }
 
 void
-    kinfo (const char *message, const char *extra) {
-	kdbgtype("INFO", message, extra);
+    kinfo (const char *message, const char *subsystem, const char *extra) {
+	kdbgtype("INFO", subsystem, message, extra);
 }

include/wt/kdebug/kdebug.h
@@ -17,16 +17,16 @@ void
 int
     kdebugf (const char *format, ...);
 void
-    kcritical (const char *message, const char *extra);
+    kcritical (const char *message, const char *subsystem, const char *extra);
 void
-    kalert (const char *message, const char *extra);
+    kalert (const char *message, const char *subsystem, const char *extra);
 void
-    kemerg (const char *message, const char *extra);
+    kemerg (const char *message, const char *subsystem, const char *extra);
 void
-    kwarn (const char *message, const char *extra);
+    kwarn (const char *message, const char *subsystem, const char *extra);
 void
-    kerror (const char *message, const char *extra);
+    kerror (const char *message, const char *subsystem, const char *extra);
 void
-    knotice (const char *message, const char *extra);
+    knotice (const char *message, const char *subsystem, const char *extra);
 void
-    kinfo (const char *message, const char *extra);
+    kinfo (const char *message, const char *subsystem, const char *extra);

commit c7a6e755568b7f421426174ba03cabd001916ddf

debug: Fix all debugging calls by adding subsystem
This commit fixes all message logging (AKA: debugging calls)
to include a subsystem.

Fixes: d737f3febfe3cab86b26a1d6f91a8467738fa56c ("debug: Add subsystem in `kdbgtype()`")
Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

Makefile
@@ -103,7 +103,7 @@ $(ISO_PATH): $(OUTDIR)/wt.elf | $(BOOTDIR)
 		$(ISODIR)
 
 # Bug reported by: John Alex (#2)
-$(DISK_PATH): 
+$(DISK_PATH):
 	./tools/create_disk.sh
 
 # Run with QEMU

kernel/core/entry/kernel.c
@@ -87,7 +87,7 @@ static void
 		struct multiboot_tag *tag = (struct multiboot_tag *) current;
 
 		if ( tag->size == 0 ) {
-			kerror("Invalid tag size (0)", KNULL);
+			kerror("Invalid tag size (0)", "multiboot", KNULL);
 			return;
 		}
 
@@ -151,7 +151,7 @@ void
 
 	kext2_set_block_device(ata_pio_read, KNULL);
 	if ( kext2_mount(0) != 0 ) {
-		kerror("EXT2 mount failed", KNULL);
+		kerror("EXT2 mount failed", "fs", KNULL);
 	}
 
 	kcpu_init_brand();

kernel/core/memory/memory_pool.c
@@ -134,7 +134,7 @@ void
 	large_pool  = pool_create(256, 128); // 32KB total
 
 	if ( !small_pool || !medium_pool || !large_pool ) {
-		kwarn("Failed to create some memory pools", KNULL);
+		kwarn("Failed to create some memory pools", "mm", KNULL);
 	}
 }
 

kernel/drivers/acpi/acpi.c
@@ -31,7 +31,7 @@ void
 
 	ksleep(POWEROFF_TIMEOUT_MS);
 	// If poweroff fails, log warning
-	kerror("Legacy poweroff failed. System may not shut down.", KNULL);
+	kerror("Legacy poweroff failed. System may not shut down.", "acpi", KNULL);
 }
 
 void
@@ -42,5 +42,5 @@ void
 
 	ksleep(POWEROFF_TIMEOUT_MS);
 	// If reboot fails, log warning
-	kerror("Legacy reboot failed. System may not restart.", KNULL);
+	kerror("Legacy reboot failed. System may not restart.", "acpi", KNULL);
 }

kernel/drivers/video/video.c
@@ -119,7 +119,7 @@ void
 	framebuffer = (volatile kuint32_t *) fb->addr;
 
 	if ( fb_info.width == 0 || fb_info.height == 0 ) {
-		kwarn("Invalid framebuffer dimensions", KNULL);
+		kwarn("Invalid framebuffer dimensions", "video", KNULL);
 		return;
 	}
 

kernel/klibc/kdebug/kdebug.c
@@ -192,12 +192,12 @@ void
 		return;
 	if ( !extra || *extra == '\0' ) {
 		if ( subsystem && *subsystem != '\0' )
-			kprintf("[%s][%s] %s\n", type, subsystem, message);
+			kprintf("[%s] %s: %s\n", type, subsystem, message);
 		else
 			kprintf("[%s] %s\n", type, message);
 	} else {
 		if ( subsystem && *subsystem != '\0' )
-			kprintf("[%s][%s] %s: %s\n", type, subsystem, message, extra);
+			kprintf("[%s] %s: %s: %s\n", type, subsystem, message, extra);
 		else
 			kprintf("[%s] %s: %s\n", type, message, extra);
 	}