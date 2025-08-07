adb00d065cca58bd4c4228114d4da909773707a6
Russian95CrE <russian95@tempestfoundation.org>
2025-08-06 23:51:13 -0300

    tools: Refactor `remove_header.sh` to work better
    Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

--- tools/remove_header.sh
+++ tools/remove_header.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
 
 find . -type f \( -name "*.c" -o -name "*.h" \) | while read -r file; do
-  if grep -q "Wind/Tempest" "$file"; then
+  if grep -qi "tempest foundation" "$file"; then
     echo "Removing header of: $file"
 
     awk '
-      BEGIN { inside_comment = 0; header_skipped = 0; printed = 0 }
+      BEGIN {
+        inside_block = 0
+        skip_block   = 0
+        printed      = 0
+      }
 
-      # Remove single-line // comment header with Wind/Tempest
-      NR == 1 && $0 ~ /^[[:space:]]*\/\/[[:space:]]*[^[:space:]]+\.(c|h)[[:space:]]*$/ { next }
-      NR <= 5 && $0 ~ /^[[:space:]]*\/\/[[:space:]]*[^[:space:]]+\.(c|h)[[:space:]]*$/ { next }
+      /^[[:space:]]*\/\/[[:space:]]*SPDX-License-Identifier:/ { next }
+      /^[[:space:]]*\/\/[[:space:]]*Copyright.*Tempest Foundation/ { next }
 
-      # Remove multi-line /* ... */ header containing Wind/Tempest
-      NR == 1 && /^\s*\/\*.*\*\/\s*$/ { next }
-      /^\/\*/ && header_skipped == 0 {
-        inside_comment = 1
-        comment_block = $0 "\n"
+      !printed && /^\s*\/\*/ {
+        inside_block = 1
+        block        = $0 "\n"
+        skip_block   = 0
         next
       }
-      inside_comment {
-        comment_block = comment_block $0 "\n"
-        if ($0 ~ /Wind\/Tempest/) {
-          skip_this = 1
-        }
+
+      inside_block {
+        block = block $0 "\n"
+        if ($0 ~ /Tempest Foundation/) skip_block = 1
         if ($0 ~ /\*\//) {
-          inside_comment = 0
-          if (skip_this) {
-            header_skipped = 1
-            skip_this = 0
+          inside_block = 0
+          if (skip_block) {
+            skip_block = 0
             next
           } else {
-            printf "%s", comment_block
+            printf "%s", block
           }
-          comment_block = ""
+          block = ""
           next
         }
         next
       }
 
-      NR <= 5 && $0 ~ /^[[:space:]]*$/ { next }
-      NR <= 5 && $0 ~ /^[[:space:]]*\/\/[[:space:]]*[^[:space:]]+\.c[[:space:]]*$/ { next }
+      !printed && ($0 ~ /^[[:space:]]*$/ || $0 ~ /^[[:space:]]*\/\/.*$/) { next }
 
       {
-        if (!printed && $0 ~ /^[[:space:]]*$/) next
         printed = 1
         print
       }
