wind-tempest/wind

commit 163926bc637d0782feabe1040828d248deda43b7

docs: Make comment work with `Better Comment` syntax
Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

sys/drivers/keyboard/keyboard.c
@@ -89,8 +89,8 @@ static void
 }
 
 /*
- * NOTE: This only works in single-tasking environments.
- * Needs locking or buffer protection for multi-tasking.
+ ! This only works in single-tasking environments.]
+ ! Needs locking or buffer protection for multi-tasking.
  */
 int
     getchar (void) {

commit d0028ffed03bd9ac24267fd03760b53e0e382b42

docs: Fix typo in comment of `keyboard.c`
Fixes: 163926bc637d0782feabe1040828d248deda43b7 ("docs: Make comment work with `Better Comment` syntax")
Signed-off-by: Russian95CrE <russian95@tempestfoundation.org>

sys/drivers/keyboard/keyboard.c
@@ -89,7 +89,7 @@ static void
 }
 
 /*
- ! This only works in single-tasking environments.]
+ ! This only works in single-tasking environments.
  ! Needs locking or buffer protection for multi-tasking.
  */
 int